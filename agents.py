c95beb7c2ba9c7a2aecb9a9a3eb8231ff6264406
Eric Abradu-Otoo <eric00@vt.edu>
2022-11-27 17:17:34 -0500

    Added movement functions to agents

--- agents.py
+++ agents.py
@@ -1,11 +1,11 @@
 import numpy as np
 import random
 import models
-import cv2
 
 class Agent:
     f = None
     pos = (0, 0)
+    prevPos = (0, 0)
     view = None
     refImg = None
     maxBounds = None
@@ -36,8 +36,47 @@ class Agent:
         self.view = self.refImg[self.pos[0]:self.pos[0]+self.f, #x
                                 self.pos[1]:self.pos[1]+self.f] #y
 
-    def move(self):
-        pass
+    def move(self, dir:str):
+        if dir == 'up':
+            self.moveUp()
+
+        elif dir == 'right':
+            self.moveRight()
+
+        elif dir == 'left':
+            self.moveLeft()
+
+        elif dir == 'down':
+            self.moveDown()
+
+    def moveUp(self):
+        self.prevPos = self.pos
+
+        if (self.pos[0] - 1) >= 0:
+            self.pos = (self.pos[0] - 1, self.pos[1])
+        self.movementVector = (self.pos[0] - self.prevPos[0], self.pos[0] - self.prevPos[0])
+
+
+    def moveRight(self):
+        self.prevPos = self.pos
+
+        if (self.pos[1] + 1) <= self.maxBounds[1]:
+            self.pos = (self.pos[0], self.pos[1] + 1)
+        self.movementVector = (self.pos[0] - self.prevPos[0], self.pos[0] - self.prevPos[0])
+
+    def moveLeft(self):
+        self.prevPos = self.pos
+
+        if (self.pos[1] - 1) >= 0:
+            self.pos = (self.pos[0], self.pos[1] - 1)
+        self.movementVector = (self.pos[0] - self.prevPos[0], self.pos[0] - self.prevPos[0])
+
+    def moveDown(self):
+        self.prevPos = self.pos
+
+        if (self.pos[0] + 1) <= self.maxBounds[0]:
+            self.pos = (self.pos[0] + 1, self.pos[1])
+        self.movementVector = (self.pos[0] - self.prevPos[0], self.pos[0] - self.prevPos[0])
 
     def randomizePosition(self):
         self.pos = (random.randint(0, self.maxBounds[0]), 
@@ -49,23 +88,24 @@ class Agent:
     def traverse(self):
 
         self.prevPos = self.pos
+        print(self.pos, self.prevPos)
         if self.moveHorizontally:
-            self.pos = (self.pos[0], self.pos[1] + 1)
+            self.moveRight()#(self.pos[0], self.pos[1] + 1)
             self.moveHorizontally = False
         else:
             self.pos = (self.pos[0]+self.__increment, self.pos[1])
 
-        if self.pos[1] > self.maxBounds[1]:
+        """if self.pos[1] > self.maxBounds[1]:
             self.pos = (self.pos[0], self.pos[1]-1)
             self.moveHorizontally = True
-            return
-
-        movementVector = (self.pos[0] - self.prevPos[0], self.pos[0] - self.prevPos[0])
+            return"""
+        
+        self.movementVector = (self.pos[0] - self.prevPos[0], self.pos[0] - self.prevPos[0])
 
         topBounds = self.pos[0] == 0
         bottomBounds = self.pos[0] == self.maxBounds[0]
         
-        if (topBounds or bottomBounds) and (np.abs(movementVector[0]) == 1):
+        if (topBounds or bottomBounds) and (np.abs(self.movementVector[0]) == 1):
             self.moveHorizontally = True
             self.__increment = -self.__increment
 
@@ -76,19 +116,29 @@ class ClassifierAgent:
 
     agents = None
 
+    locationTensor = None
+
+    infoSpace = None
+
     def __init__(self, image=np.zeros((28, 28, 1)), numAgents=2, windowLen=2):
         self.newImg = np.zeros(image.shape)
+        shape = (image.shape[0:2])
+        print(shape)
+        self.locationTensor = np.zeros(shape, dtype=np.float32)
+        self.infoSpace = np.ones(shape, dtype=np.float32)
 
         self.agents = [Agent(image, windowLen) for i in range(numAgents)]
-
         print(f"Number of agents: {len(self.agents)}")
+        
+        self.erase = np.zeros((windowLen, windowLen), dtype=np.float32)
+
         self.initModel()
 
     def capture(self):
         for agent in self.agents:
             agent.capture()
-            pos = agent.pos
-            self.newImg[pos[0]:pos[0]+agent.f, pos[1]:pos[1]+agent.f, :] = agent.view/255.0
+            self.updateNewImg(agent)
+            self.updateInfoSpace(agent)
 
     def sampleImage(self, image):
         #samImg = np.zeros(image.shape)
@@ -101,11 +151,33 @@ class ClassifierAgent:
 
         return self.newImg
 
+    # Function should move all agents across an image appropriately
+    def traverseAgents(self):
+        for agent in self.agents:
+            agent.traverse()
+            self.updateLocTensor(agent)
+
+        #self.capture()
+
 
     # Function should move all agents across an image appropriately
     def moveAgents(self):
+        for agent in self.agents:
+            agent.move()
+            self.updateLocTensor(agent)
+        
+        #self.capture()
         pass
 
+    def updateLocTensor(self, agent:Agent):
+        self.locationTensor[agent.prevPos[0], agent.prevPos[1]] = 0
+        self.locationTensor[agent.pos[0], agent.pos[1]] = 1
+
+    def updateInfoSpace(self, agent:Agent):
+        self.infoSpace[agent.pos[0]:agent.pos[0]+agent.f, agent.pos[1]:agent.pos[1]+agent.f] = self.erase
+
+    def updateNewImg(self, agent:Agent):
+        self.newImg[agent.pos[0]:agent.pos[0]+agent.f, agent.pos[1]:agent.pos[1]+agent.f, :] = agent.view/255.0
 
     def initModel(self):
         self.model = models.create_CNN_model(self.newImg.shape, 10)
--- testcases.py
+++ testcases.py
@@ -7,10 +7,11 @@ image = cv.imread("trainingSample/img_569.jpg")
 
 #dim = image.shape
 dimResized = (512, 512)
-
-#a = Agent(image, 6)
-#b = Agent(image, 6)
 mainDude = ClassifierAgent(image, numAgents=2, windowLen=2)
+
+# Test agents 
+"""
+
 mainDude.capture()
 
 #a.capture()
@@ -20,6 +21,7 @@ cv.imshow("Sample Image", cv.resize(image, dimResized, interpolation=cv.INTER_AR
 cv.imshow("Agent a's view", cv.resize(mainDude.agents[0].view, dimResized, interpolation=cv.INTER_AREA))
 cv.imshow("Agent b's view", cv.resize(mainDude.agents[1].view, dimResized, interpolation=cv.INTER_AREA))
 cv.waitKey()
+"""
 
 ## Test Traverse Function. Should carry the agent through every possible pixel location it can
 ## occupy based on its window/view size
@@ -39,30 +41,79 @@ for i in range(a.maxBounds[0]*a.maxBounds[1] + 5):
 ## Test Classifier's reconstruction based on agent(s)
 """
 image[27, 27] = (255, 0, 0)
-a.pos = (0, 0)
-b.pos = (0, 15)
-mainDude.update(a, b)
-print("Test Classifier's reconstruction function\n", a.pos)
-for i in range(int((a.maxBounds[0]+1)*(a.maxBfunds[1]+1)/2)):
-    a.traverse()
-    b.traverse()
-    mainDude.update(a, b)
+mainDude.agents[0].pos = (0, 0)
+mainDude.agents[1].pos = (0, 14)
+
+mainDude.capture()
+mainDude.traverseAgents()
+print("Test Classifier's reconstruction function\n", mainDude.agents[0].pos)
+for i in range(int(28*28/len(mainDude.agents))):
+    mainDude.capture()
+    mainDude.traverseAgents()
 
     positionalImg = image.copy()
-    positionalImg[a.pos[0], a.pos[1]] = (0, 0, 255)
+
+    for agent in mainDude.agents:
+        positionalImg[agent.pos[0], agent.pos[1]] = (0, 0, 255)
+    
 
     cv.imshow("Positional Image", cv.resize(positionalImg, dimResized, interpolation=cv.INTER_AREA))
     cv.imshow("Reconstructed Image", cv.resize(mainDude.newImg, dimResized, interpolation=cv.INTER_AREA))
+    cv.imshow("Information Space", cv.resize(mainDude.infoSpace, dimResized, interpolation=cv.INTER_AREA))
 
-    print(f"Agent a position: {a.pos} | Agent b position: {b.pos}")
+    print(f"Agent a position: {mainDude.agents[0].pos} | Agent b position: {mainDude.agents[1].pos}")
     cv.waitKey(30)
 
 cv.imshow("Reconstructed Image", cv.resize(mainDude.newImg, dimResized, interpolation=cv.INTER_AREA))
 cv.waitKey()
-
 """
 
+# Test Agent movement functions
+
+classifier = ClassifierAgent(image/255.0, numAgents=4, windowLen=3)
+
+a = classifier.agents[0]
+b = classifier.agents[1]
+c = classifier.agents[2]
+d = classifier.agents[3]
+
+a.pos = (0, 0)
+b.pos = (0, 25)
+c.pos = (25, 0)
+d.pos = (25, 25)
+
+for agent in classifier.agents:
+    print(f"Agent {agent}' position: {agent.pos}")
+
+print("Test Horizontal Bounds")
+a.moveLeft()
+b.moveRight()
+c.moveLeft()
+d.moveRight()
+
+for agent in classifier.agents:
+    print(f"Agent {agent}' in bounds?: {agent.pos}, {agent.pos == agent.prevPos}")
+
+print("Test Vertical Bounds")
+a.moveUp()
+b.moveUp()
+c.moveDown()
+d.moveDown()
+
+for agent in classifier.agents:
+    print(f"Agent {agent}' in bounds?: {agent.pos}, {agent.pos == agent.prevPos}")
+
+a.moveDown()
+a.moveRight()
+print(f"Agent a moved diagonally?: {a.pos}, {a.pos == (1, 1)}")
+
+d.moveUp()
+d.moveLeft()
+print(f"Agent d moved diagonally?: {d.pos}, {d.pos == (24, 24)}")
+
+
 # Tests the Sampling function by the Classifier Agent
+"""
 image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 classifier = ClassifierAgent(image/255.0, numAgents=4, windowLen=6)
@@ -75,3 +126,4 @@ print(f"Input image dimensions: {image.shape}, data type: {image.dtype} | \
 cv.imshow("Sampled Image", cv.resize(i, dimResized, interpolation=cv.INTER_AREA))
 
 cv.waitKey()
+"""
